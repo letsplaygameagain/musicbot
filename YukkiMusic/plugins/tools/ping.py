a54f0d00cce662165d7589683cdbda78979c55f4
Robotikaazazel <73426077+Robotikaazazel@users.noreply.github.com>
2023-07-05 14:04:48 +0700

    update ping.py

--- YukkiMusic/plugins/tools/ping.py
+++ YukkiMusic/plugins/tools/ping.py
@@ -14,10 +14,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils import bot_sys_stats
-from YukkiMusic.utils.decorators.language import language
+from JooxMusic import app
+from JooxMusic.core.call import Joox
+from JooxMusic.utils import bot_sys_stats
+from JooxMusic.utils.decorators.language import language
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
@@ -36,7 +36,7 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"],
     )
     start = datetime.now()
-    pytgping = await Yukki.ping()
+    pytgping = await Joox.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
