16ccf07ef83f527dbe00a5d1c17df638a1d8cbd4
Robotikaazazel <73426077+Robotikaazazel@users.noreply.github.com>
2023-07-05 13:06:06 +0700

    update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -15,11 +15,11 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import help_pannel
-from YukkiMusic.utils.database import get_lang, is_commanddelete_on
-from YukkiMusic.utils.decorators.language import (LanguageStart,
+from JooxMusic import app
+from JooxMusic.misc import SUDOERS
+from JooxMusic.utils import help_pannel
+from JooxMusic.utils.database import get_lang, is_commanddelete_on
+from JooxMusic.utils.decorators.language import (LanguageStart,
                                                   languageCB)
 from YukkiMusic.utils.inline.help import (help_back_markup,
                                           private_help_panel)
