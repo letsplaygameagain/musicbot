7a66d417249e3647a026412beabdfb915cd27bec
Robotikaazazel <73426077+Robotikaazazel@users.noreply.github.com>
2023-07-05 13:57:33 +0700

    update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -19,21 +19,21 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from JooxMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils import seconds_to_min, time_to_seconds
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.database import is_video_allowed
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.decorators.play import PlayWrapper
-from YukkiMusic.utils.formatters import formats
-from YukkiMusic.utils.inline.play import (livestream_markup,
+from JooxMusic.core.call import Joox
+from JooxMusic.utils import seconds_to_min, time_to_seconds
+from JooxMusic.utils.channelplay import get_channeplayCB
+from JooxMusic.utils.database import is_video_allowed
+from JooxMusic.utils.decorators.language import languageCB
+from JooxMusic.utils.decorators.play import PlayWrapper
+from JooxMusic.utils.formatters import formats
+from JooxMusic.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from YukkiMusic.utils.inline.playlist import botplaylist_markup
-from YukkiMusic.utils.logger import play_logs
-from YukkiMusic.utils.stream.stream import stream
+from JooxMusic.utils.inline.playlist import botplaylist_markup
+from JooxMusic.utils.logger import play_logs
+from JooxMusic.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
